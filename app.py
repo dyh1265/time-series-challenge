03320ba36550f5a803f5219199a461ab49f3f06a
dyh1265 <Andrei.Sirazitdinov@medma.uni-heidelberg.de>
2025-06-11 09:55:19 +0200

    Updated model loading

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the model tensorlfow model from a file
-tf_model = tf.keras.models.load_model('models/alcohol_accidents_prediction_model.h5')
+tf_model = tf.keras.models.load_model('models/alcohol_accidents_prediction_model.h5',
+                                      custom_objects={'mse': tf.keras.losses.MeanSquaredError()})
 model = keras.models.load_model('models/model.keras')
 
 
